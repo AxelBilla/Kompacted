37b7de898199eac23f4422daab2a1a7d61f4e9b1
Axel B. <144888254+AxelBilla@users.noreply.github.com>
2025-10-11 10:04:49 +0200

    Added forEach
    Added the ability to iterate Kompacts over data arrays (doesn't work)

--- samples/sample_script.js
+++ samples/sample_script.js
@@ -1,16 +1,40 @@
+class User{
+    constructor(name, email){
+        this.name=name;
+        this.email=email;
+    }
+}
+
+const users = [
+    new User("john", "notwit@gmail.com"),
+    new User("remie", "baba@nihon.jp"),
+    new User("bella", "kanougo.deria@free.fr")
+]
+
 window.addEventListener("load", ()=>{
     const KOMPACTED = new Kompacted();
     
-    KOMPACTED.set((kmptd)=>{
-        kmptd.new("test_comp_one",
+    KOMPACTED.new((kmptd)=>{
+        kmptd.add("test_comp_one",
             `
             <div onmouseover="console.log('hey')">
-                <p>test_comp_one</p>
+                <p>to_be_replaced</p>
+                <p>to_be_replaced</p>
             </div>
-            `
+            `,
+            "load", (self)=>{
+                let index = self.getAttribute('data');
+                let info = self.getAttribute('info');
+                let ps = self.getElementsByTagName('p');
+                let user = users[index];
+                
+                ps[0].innerText=user.name;
+                if(info!=null) ps[0].innerText+=` (${info})`;
+                ps[1].innerText=user.email;
+            }
         );
         
-        KOMPACTED.new("test_comp_two",
+        KOMPACTED.add("test_comp_two",
             `
             <p>test_comp_two</p>
             `,
@@ -18,12 +42,29 @@ window.addEventListener("load", ()=>{
                 KOMPACTED.edit("test_comp_two",
                         `
                         <div>
-                            <p>defe</p>
+                            <p>Changed!</p>
                         </div>
                         `
                     ); 
                 
                 KOMPACTED.load(document, true);
+            }
+        );
+        
+        kmptd.set("users", users);
+        
+        kmptd.add("test_comp_three",
+            `
+            <h1>forEach</h1>
+            <p>name</p>
+            <p>email</p>
+            `,
+            "load", (self)=>{
+                let name = self.getAttribute('name');
+                let email = self.getAttribute('email');
+                let ps = self.getElementsByTagName('p');
+                ps[0].innerText=name;
+                ps[1].innerText=email;
             }
         );
     }, document.getElementById("twos"))  
