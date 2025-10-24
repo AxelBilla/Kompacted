da04628ed88cf8e86ad962f78e84c04557924b3c
Axel B. <144888254+AxelBilla@users.noreply.github.com>
2025-10-24 18:06:44 +0200

    Fixed Foreach/Kompact Loading
    Fixed a bug in which Kompact and Foreach HTML tags were reliant on the loading order of their Komp's script in the HTML header (could not swap X & Y in the same div without X not being loaded).
    
    Added the ability to load multiple instances of Kompacted at once.
    
    Added the ability to check if an instance of Kompacted possesses a given Data Array by its identifier.
    Added the ability to check if an instance of Kompacted possesses a given Template.

--- kompacted.js
+++ kompacted.js
@@ -30,6 +30,13 @@ class Kompacted{
         this.loadForeach(scope, deep);
     }
     
+    // (re)Loads all the Komps within a given scope of all the given instances of Kompacted 
+    static load(scope, deep, ...kompacted){
+        for(let KMPTD of kompacted[0]) {
+            KMPTD.load(scope, deep)
+        }
+    }
+    
     // Sets a Data array to a given identifier
     set(id, data_array){
        this.setData(id, data_array); 
@@ -44,16 +51,22 @@ class Kompacted{
     // Gets all Kompact tags within a given scope and turns them into Komps
     loadKompacts(scope, deep=false){
         let kompacts = scope.getElementsByTagName(Kompacted.DefaultValues.KOMPACT_HTML_TAG);
-
+    
         if(!deep) {
             for (let i = 0; i < kompacts.length; i++) {
+                if (!this.hasTemplate(kompacts[i].getAttribute(Kompacted.DefaultValues.KOMPACT_NAME_ATTRIBUTE))) continue;
                 let komp = this.getKomp(kompacts[i]);
                 this.setKomp(kompacts[i], komp, deep);
             }
         } else {
-            while (kompacts.length !== 0) {
-                let komp = this.getKomp(kompacts[0]);
-                this.setKomp(kompacts[0], komp, deep);
+            let skips = 0;
+            while (kompacts.length!==0 && skips<kompacts.length) {
+                if (!this.hasTemplate(kompacts[skips].getAttribute(Kompacted.DefaultValues.KOMPACT_NAME_ATTRIBUTE))) {
+                    skips++;
+                    continue;
+                }
+                let komp = this.getKomp(kompacts[skips]);
+                this.setKomp(kompacts[skips], komp, deep);
             }
         }
     }
@@ -61,9 +74,9 @@ class Kompacted{
     // Gets all Foreach tags within a given scope and turns them into a list of given Komps
     loadForeach(scope, deep=false){
         let foreach = scope.getElementsByTagName(Kompacted.DefaultValues.FOREACH_HTML_TAG);
-        
         if(!deep) {
             for (let i = 0; i < foreach.length; i++) {
+                if (!this.hasTemplate(foreach[i].getAttribute(Kompacted.DefaultValues.FOREACH_AS_KOMP_ATTRIBUTE))) continue;
                 let hasData = foreach[i].hasAttribute(Kompacted.DefaultValues.FOREACH_SOURCE_ATTRIBUTE);
                 let hasCount = foreach[i].hasAttribute(Kompacted.DefaultValues.FOREACH_COUNT_ATTRIBUTE);
                 if(!(hasData || hasCount)) throw Kompacted.Errors.INVALID_HTML_TAG+` (${target})`
@@ -72,13 +85,18 @@ class Kompacted{
                 this.setKomps(foreach[i], data, deep);
             }
         } else {
-            while (foreach.length !== 0) {
-                let hasData = foreach[0].hasAttribute(Kompacted.DefaultValues.FOREACH_SOURCE_ATTRIBUTE);
-                let hasCount = foreach[0].hasAttribute(Kompacted.DefaultValues.FOREACH_COUNT_ATTRIBUTE);
+            let skips = 0;
+            while (foreach.length!==0 && skips<foreach.length) {
+                if (!this.hasTemplate(foreach[skips].getAttribute(Kompacted.DefaultValues.FOREACH_AS_KOMP_ATTRIBUTE))) {
+                    skips++;
+                    continue;
+                }
+                let hasData = foreach[skips].hasAttribute(Kompacted.DefaultValues.FOREACH_SOURCE_ATTRIBUTE);
+                let hasCount = foreach[skips].hasAttribute(Kompacted.DefaultValues.FOREACH_COUNT_ATTRIBUTE);
                 if(!(hasData || hasCount)) throw Kompacted.Errors.INVALID_HTML_TAG+` (${target})`
                 
-                let data = (hasData) ? this.getData(foreach[0].getAttribute(Kompacted.DefaultValues.FOREACH_SOURCE_ATTRIBUTE)) : {[Kompacted.DefaultValues.FOREACH_COUNT_ATTRIBUTE]: foreach[0].getAttribute(Kompacted.DefaultValues.FOREACH_COUNT_ATTRIBUTE)};
-                this.setKomps(foreach[0], data, deep);
+                let data = (hasData) ? this.getData(foreach[skips].getAttribute(Kompacted.DefaultValues.FOREACH_SOURCE_ATTRIBUTE)) : {[Kompacted.DefaultValues.FOREACH_COUNT_ATTRIBUTE]: foreach[skips].getAttribute(Kompacted.DefaultValues.FOREACH_COUNT_ATTRIBUTE)};
+                this.setKomps(foreach[skips], data, deep);
             }
         }
     }
@@ -92,7 +110,6 @@ class Kompacted{
     // Adds a given amount (count/data amount) of nodes of a given Komp
     setKomps(target, data, deep=false){
         let komp_name = target.getAttribute(Kompacted.DefaultValues.FOREACH_AS_KOMP_ATTRIBUTE);
-        
         const appendKomp = (target, komp, deep)=>{
             if(!deep) target.appendChild(komp);
             else target.parentNode.insertBefore(komp, target)
@@ -136,7 +153,6 @@ class Kompacted{
     createKomp(template, data=undefined, origin_kompact=undefined){
         let komp = document.createElement(template.name);
         komp.innerHTML = template.html;
-        
         
         if(!isNull(data) && !data.hasOwnProperty(Kompacted.DefaultValues.FOREACH_COUNT_ATTRIBUTE)){
             if(typeof(data)!==typeof({})){
@@ -195,10 +211,15 @@ class Kompacted{
 
     // Replace a given template in the saved list with a new one (if it exists)
     editTemplate(name, template){
-        if(!this.template_list.hasOwnProperty(name)) {
+        if(!this.hasTemplate(name)) {
             throw Kompacted.Errors.VALUE_NOT_FOUND+` ('${name}') `;
         }
         this.template_list[template.name] = template;
+    }
+    
+    // Checks if a template exists for a given name
+    hasTemplate(name){
+        return this.template_list.hasOwnProperty(name);
     }
 
     // Saves all templates created
@@ -213,10 +234,15 @@ class Kompacted{
     
     // Get a Data Array by its identifier
     getData(identifier){
-        if(!this.stored_data_arrays.hasOwnProperty(identifier)) {
+        if(!this.hasData(identifier)) {
             throw Kompacted.Errors.VALUE_NOT_FOUND+` ('${identifier}') `;
         }
         return this.stored_data_arrays[identifier];
+    }
+    
+    // Checks if a Data Array exists for a given identifier
+    hasData(identifier){
+        return this.stored_data_arrays.hasOwnProperty(identifier);
     }
 
     // Saves all necessary Data Arrays
